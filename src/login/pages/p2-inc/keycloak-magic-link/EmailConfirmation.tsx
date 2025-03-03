87260988cfbcae0eca0c9cd7828827334466411d
paulwer <paul@wer-ner.de>
2025-03-03 15:10:00 +0100

    fixes

--- src/login/pages/p2-inc/keycloak-magic-link/EmailConfirmation.tsx
+++ src/login/pages/p2-inc/keycloak-magic-link/EmailConfirmation.tsx
@@ -1,8 +1,6 @@
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../../../KcContext";
 import type { I18n } from "../../../i18n";
-import { clsx } from "keycloakify/tools/clsx";
-import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 
 export default function EmailConfirmation(props: PageProps<Extract<KcContext, { pageId: "email-confirmation.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
